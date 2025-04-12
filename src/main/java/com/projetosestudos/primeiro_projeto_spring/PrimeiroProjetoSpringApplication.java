a707ccfed4d7c094ce182209f6c07828169f3965
Denzel <odenzel949@gmail.com>
2025-04-12 19:21:36 -0300

    Project created

---
+++ src/main/java/com/projetosestudos/primeiro_projeto_spring/PrimeiroProjetoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.projetosestudos.primeiro_projeto_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroProjetoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroProjetoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetosestudos/primeiro_projeto_spring/PrimeiroProjetoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetosestudos.primeiro_projeto_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroProjetoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
