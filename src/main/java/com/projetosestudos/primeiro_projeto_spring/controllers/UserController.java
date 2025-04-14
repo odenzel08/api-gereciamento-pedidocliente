82eeb01e3a30c57bbc534f9cc243b5b6f8448ac0
Denzel <odenzel949@gmail.com>
2025-04-13 22:23:06 -0300

    User entity and controller

---
+++ src/main/java/com/projetosestudos/primeiro_projeto_spring/controllers/UserController.java
@@ -0,0 +1,19 @@
+package com.projetosestudos.primeiro_projeto_spring.controllers;
+
+import com.projetosestudos.primeiro_projeto_spring.model.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User user = new User(1L, "Maria", "maria@gmail.com",
+                "99999-8888", "12345");
+        return ResponseEntity.ok().body(user);
+    }
+}
