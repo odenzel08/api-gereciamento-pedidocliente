e5e354954e6e46028d3e6014508e634b3ef05c0a
Denzel <odenzel949@gmail.com>
2025-04-15 21:54:09 -0300

    Criação do banco de dados H2, e mapeamento da classe 'User'

--- src/main/java/com/projetosestudos/primeiro_projeto_spring/model/User.java
+++ src/main/java/com/projetosestudos/primeiro_projeto_spring/model/User.java
@@ -1,11 +1,18 @@
 package com.projetosestudos.primeiro_projeto_spring.model;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
     private String email;
     private String phone;
